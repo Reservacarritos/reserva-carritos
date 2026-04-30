ce970c3e7eff66a5c396e20b2ecca0ba2ba7dbc6
Reservacarritos <reserva.carritos@gmail.com>
2026-04-30 15:17:39 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reserva-carritos-v1';
+const CACHE_NAME = 'reserva-carritos-v2';
 const FILES = ['/reserva-carritos/', '/reserva-carritos/index.html'];
 
 self.addEventListener('install', e => {
